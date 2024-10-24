96b1ec645c2761eb6560e4c7ea961d3450308676
Facephi <developer@facephi.com>
2024-10-24 14:43:18 +0000

    Add frameworks and resources

---
+++ FPhiSelphIDWidgetiOS.xcframework/ios-x86_64-simulator/FPhiSelphIDWidgetiOS.framework/Headers/FPhiSDRDocumentExtractor.h
@@ -0,0 +1,9 @@
+#ifndef FPhiSDRDocumentExtractor_h
+#define FPhiSDRDocumentExtractor_h
+
+#import "IFPhiDocumentExtractor.h"
+
+
+@interface FPhiSDRDocumentExtractor : IFPhiDocumentExtractor
+@end
+#endif
