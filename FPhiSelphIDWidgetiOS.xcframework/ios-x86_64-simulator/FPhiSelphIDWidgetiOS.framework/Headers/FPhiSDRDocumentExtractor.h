209357499456682b86f0bb96409b22133ea12a52
Facephi <developer@facephi.com>
2025-01-20 15:18:16 +0000

    Delete frameworks and resources

--- FPhiSelphIDWidgetiOS.xcframework/ios-x86_64-simulator/FPhiSelphIDWidgetiOS.framework/Headers/FPhiSDRDocumentExtractor.h
+++
@@ -1,9 +0,0 @@
-#ifndef FPhiSDRDocumentExtractor_h
-#define FPhiSDRDocumentExtractor_h
-
-#import "IFPhiDocumentExtractor.h"
-
-
-@interface FPhiSDRDocumentExtractor : IFPhiDocumentExtractor
-@end
-#endif
